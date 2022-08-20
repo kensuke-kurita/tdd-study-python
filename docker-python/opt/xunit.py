4855583e07682f5f428b7670ed73dec76c1a7390
kurita_k <kensuke.kurita@gmail.com>
2022-08-20 16:48:11 +0900

    21章

--- docker-python/opt/xunit.py
+++ docker-python/opt/xunit.py
@@ -1,3 +1,10 @@
+class testResult:
+  def __init__(self):
+    self.runCount = 0
+  def testStarted(self):
+    self.runCount = self.runCount + 1
+  def summary(self):
+    return "%d run, 0 failed" % self.runCount
 class TestCase:
   def __init__(self, name):
     self.name = name
@@ -6,15 +13,20 @@ class TestCase:
   def tearDown(self):
     pass
   def run(self):
+      result = testResult()
+      result.testStarted()
       self.setUp()
       method = getattr(self, self.name)
       method()
       self.tearDown()
+      return result
 class WasRun(TestCase):
   def setUp(self):
     self.log = "SetUp "
   def testMethod(self):
     self.log = self.log + "testMethod "
+  def testBrokenMethod(self):
+    raise Exception
   def tearDown(self):
     self.log = self.log + "tearDown "
 class TestCaseTest(TestCase):
@@ -22,8 +34,19 @@ class TestCaseTest(TestCase):
     test = WasRun("testMethod")
     test.run()
     assert("SetUp testMethod tearDown " == test.log)
+  def testResult(self):
+    test = WasRun("testMethod")
+    result = test.run()
+    assert("1 run, 0 failed" == result.summary())
+  def testFailedResult(self):
+    test = WasRun("testBrokenMethod")
+    result = test.run()
+    assert("1 run, 1 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
+
 ## クラスを先に読み込んでいなかった。
 
 #- パフォーマンスと独立性
